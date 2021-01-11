0b2f6e329e866eefae4cc61b965b081e38829325
David Kilonzo <binginaya708@gmail.com>
2021-01-12 00:22:41 +0300

    changes to heroku

--- Mae_Gallary/settings.py
+++ Mae_Gallary/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from decouple import config,Csv
+import django_heroku
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,14 +82,23 @@ WSGI_APPLICATION = 'Mae_Gallary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  'gallary',
-        'USER':  'moringa',
-        'PASSWORD': 'Access',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':  'gallary',
+#         'USER':  'moringa',
+#         'PASSWORD': 'Access',
+#     }
+# }
+
+
+# production
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
 
 
 # Password validation
