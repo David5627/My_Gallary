a12bca400f926709150feb6a933930ac2c479be3
David Kilonzo <binginaya708@gmail.com>
2021-01-12 01:33:45 +0300

    changes to heroku

--- Mae_Gallary/settings.py
+++ Mae_Gallary/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'Mae_Gallary.wsgi.application'
 
 
 # production
-   DATABASES = {
+DATABASES = {
        'default': dj_database_url.config(
            default=config('DATABASE_URL')
        )
