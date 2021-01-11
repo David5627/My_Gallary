77c6e0d6e50df7f2536b5e283c022df193ef6345
David Kilonzo <binginaya708@gmail.com>
2021-01-11 16:33:50 +0300

    created function for pictures

--- pictures/views.py
+++ pictures/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
 from .models import Image,Location
+
+
+def pictures(request):
+    pics = Image.objects.all()
+    locations = Location.objects.all()
+    return render(request,'photos.html',{'pics':pics,'locations':locations})
