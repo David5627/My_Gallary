8bc5e3c678afff163e5f41ea7faa3b9a13c79aa5
David Kilonzo <binginaya708@gmail.com>
2021-01-11 16:09:38 +0300

    added saves on location

--- pictures/models.py
+++ pictures/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
     locationName = models.CharField(max_length=30)
+
+
+    def saveLocation(self):
+        self.save()
